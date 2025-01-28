4a347b06ee76a48b4910fa7237645d5e2ee99605
Mikolaj231 <mikolajmelnyk16@gmail.com>
2025-01-28 18:17:40 +0100

    1.9 Update and fix frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import 'devicon/devicon.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import Background from "./components/layout/Background";
 import Routing from './routing'; // Importujemy Routing
 import { HelmetProvider } from 'react-helmet-async';
 import './App.css';
@@ -11,7 +10,6 @@ function App() {
   return (
     <HelmetProvider>
       <Router>
-        <Background />
 
         <Layout>
           <Routing />
